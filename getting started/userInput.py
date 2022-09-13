218f8ec82115b6abb475a5fa1eb822d0f6e1e665
Fien-Dora <fiendora5@gmail.com>
2022-09-13 23:12:20 +0100

    modified dailog box

--- getting started/userInput.py
+++ getting started/userInput.py
@@ -1,5 +1,5 @@
 from kivymd.app import MDApp
-from kivymd.uix.screen import Screen
+# from kivymd.uix.screen import Screen
 from kivy.lang import Builder
 from  kivymd.uix.button import MDRectangleFlatButton
 from kivymd.uix.dialog import MDDialog
@@ -26,8 +26,18 @@ class DemoApp(MDApp):
         return screen
 
     def show_data(self, obj):
-        dailog = MDDialog(title='Username',text=self.username.text,
-                            size_hint=(0.55, 1))
-        dailog.open()
+        if self.username.text == "":
+            check_string = "Please enter Username"
+        else:
+            check_string = self.username.text + " does not exist"
+        close_button = MDRectangleFlatButton(text = 'close', on_release=self.close_dailog)
+        more_button = MDRectangleFlatButton(text = 'more')
+        self.dailog = MDDialog(title='Username',text=check_string,
+                            size_hint=(0.55, 1),
+                            buttons=[close_button, more_button])
+        self.dailog.open()
+
+    def close_dailog(self, odj):
+        self.dailog.dismiss()
 
 DemoApp().run()
