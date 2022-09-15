cb8771b47e8d0d015aac1c0ded4fef151582b5b4
MLesky <96002447+MLesky@users.noreply.github.com>
2022-09-14 17:23:17 -0700

    Add files via upload
    added navigation drawer

--- Device_menu/main.py
+++ Device_menu/main.py
@@ -9,29 +9,79 @@ from random import randint
 Window.size = (300, 600)
 
 loader = """
-Screen:
-    BoxLayout:
-        orientation: 'vertical'
+MDScreen: 
+    MDNavigationLayout:
+        ScreenManager:
+            MDScreen:
+                MDBoxLayout:
+                    orientation: 'vertical'
 
-        MDTopAppBar:
-            title: 'DEVICES'
-            left_action_items: [['menu', lambda x: app.nav_draw()]]
-            right_action_items: [['card-search', lambda x: app.search_btn()]]
-            elevation: 20
-            opposite_colors: True
+                    MDTopAppBar:
+                        title: 'DEVICES'
+                        left_action_items: [['menu', lambda x: nav_drawer.set_state('toggle')]]
+                        right_action_items: [['card-search', lambda x: app.search_btn()]]
+                        elevation: 20
+                        opposite_colors: True
 
-        ScrollView:
-            MDList:
-                id: container
+                    ScrollView:
+                        MDList:
+                            OneLineIconListItem:
+                                text: 'Add New Device'
+                                IconLeftWidget:
+                                    icon: 'plus'
 
-        MDBottomAppBar:
-            MDTopAppBar:
-                left_action_items: [['devices', lambda x: app.all_devices()],['wifi', lambda x: app.wifi_devices()], ['bluetooth', lambda x: app.bt_devices()], ['web', lambda x: app.web_devices()], ['cable-data', lambda x: app.wired_devices()], ['remote', lambda x: app.ir_devices()]]
-                opposite_colors: True
-                mode: 'free-end'
-                type: 'bottom'
-                icon: 'plus'
-                on_action_button: app.add_device()
+                    MDBottomAppBar:
+                        MDTopAppBar:
+                            left_action_items: [['devices', lambda x: app.all_devices()],['wifi', lambda x: app.wifi_devices()], ['bluetooth', lambda x: app.bt_devices()], ['web', lambda x: app.web_devices()], ['cable-data', lambda x: app.wired_devices()], ['remote', lambda x: app.ir_devices()]]
+                            opposite_colors: True
+                            mode: 'free-end'
+                            type: 'bottom'
+                            icon: 'plus'
+                            on_action_button: app.add_device()
+                                
+                    Widget:
+
+        MDNavigationDrawer:
+            id: nav_drawer
+            MDBoxLayout:
+                orientation: 'vertical'
+
+                Image:
+                    source: 'assets/images/remote.jpg'
+                    size_hint: (0.25, 0.25)
+
+                MDLabel:
+                    text: 'Remote Devices'
+                    halign: 'left'
+                    size_hint_y: None
+                    font_style: 'H6'
+                    height: self.texture_size[1]
+
+                ScrollView:
+                    MDList:
+                        OneLineIconListItem:
+                            text: 'Home'
+                            IconLeftWidget:
+                                icon: 'home'
+                        
+                        OneLineIconListItem:
+                            text: 'Controller'
+                            IconLeftWidget:
+                                icon: 'controller'
+
+                        OneLineIconListItem:
+                            text: 'Devices'
+                            IconLeftWidget:
+                                icon: 'devices'
+
+                MDBoxLayout:
+                    orientation: 'horizontal'
+
+                    MDIcon:
+                        icon: 'account-circle'
+
+                    MDIcon:
+                        icon: 'brightness-7'
 """
 
 class MyApp(MDApp):
@@ -39,21 +89,6 @@ class MyApp(MDApp):
         self.theme_cls.primary_palette = 'Green'
         screen = Builder.load_string(loader)
         return screen
-
-    def on_start(self):
-        item = OneLineIconListItem(text = 'Add New Device')
-        item.add_widget(IconLeftWidget(icon = 'plus'))
-        self.root.ids.container.add_widget(item)
-        for x in range(18):
-            i = randint(0, 17)
-            icon = IconLeftWidget(icon = icons[i])
-            item = ThreeLineIconListItem(
-                text = names[i],
-                secondary_text = types[i],
-                tertiary_text = modes[randint(0, 4)]
-            )
-            item.add_widget(icon)
-            self.root.ids.container.add_widget(item)
 
     def nav_draw(self):
         print("Navigation Drawer")
